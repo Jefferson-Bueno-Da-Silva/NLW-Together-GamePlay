f0045d106a100cc6275e8f86a3d280f17eac45d2
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-06-23 20:59:46 -0300

    add flat list

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -9,9 +9,25 @@ import { ListHeader } from '../../components/ListHeader';
 
 // styles
 import { styles } from './styles';
+import { FlatList } from 'react-native';
 
 export function Home() {
     const [ category, setCategory ] = useState('');
+
+    const appointments = [
+        {
+            id: '1',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: "22/06 às 20:40h",
+            description: "É hoje que vamos chegar ao challenger sem perder uma partida da md10"
+        }
+    ]
 
     function handleCategorySelect(categoryId : string){
         categoryId === category ? setCategory('') : setCategory(categoryId);
@@ -35,6 +51,14 @@ export function Home() {
                 <ListHeader
                     title="Partidas agendadas"
                     subTitle="Total 6"
+                />
+
+                <FlatList
+                    data={appointments}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => (
+                        <Text>{item.guild.name}</Text>
+                    )}
                 />
             </View>
 
