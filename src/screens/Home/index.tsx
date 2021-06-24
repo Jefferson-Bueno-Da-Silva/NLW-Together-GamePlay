deec887f6d161e5bc23f8a43c76366c80521c7fe
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-06-24 08:25:59 -0300

    add listDivider and Appointment to Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,6 +6,8 @@ import { Profile } from '../../components/Profile';
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
 import { ListHeader } from '../../components/ListHeader';
+import { Appointment } from '../../components/Appointment';
+import { ListDivider } from '../../components/ListDivider';
 
 // styles
 import { styles } from './styles';
@@ -26,6 +28,18 @@ export function Home() {
             category: '1',
             date: "22/06 às 20:40h",
             description: "É hoje que vamos chegar ao challenger sem perder uma partida da md10"
+        },
+        {
+            id: '2',
+            guild: {
+                id: '2',
+                name: 'Lendários',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: "22/06 às 20:40h",
+            description: "É hoje que vamos chegar ao challenger sem perder uma partida da md10"
         }
     ]
 
@@ -34,18 +48,16 @@ export function Home() {
     }
 
     return (
-        <View style={styles.constrainer} >
+        <View>
             <View style={styles.header}>
                 <Profile />
                 <ButtonAdd />
             </View>
-
-            <View>
-                <CategorySelect
-                    categorySelected={category}
-                    setCategory={handleCategorySelect}
-                />
-            </View>
+            
+            <CategorySelect
+                categorySelected={category}
+                setCategory={handleCategorySelect}
+            />
 
             <View style={styles.content} >
                 <ListHeader
@@ -56,12 +68,15 @@ export function Home() {
                 <FlatList
                     data={appointments}
                     keyExtractor={item => item.id}
+                    style={styles.matches}
+                    showsVerticalScrollIndicator={false}
                     renderItem={({ item }) => (
-                        <Text>{item.guild.name}</Text>
+                        <Appointment data={item} />
                     )}
+                    ItemSeparatorComponent={() => <ListDivider />}
                 />
-            </View>
 
+            </View>
         </View>
     )
 }
