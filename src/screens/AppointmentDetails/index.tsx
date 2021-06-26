a363865332024c2fac3896feb21fddf2c788ba52
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-06-26 11:54:31 -0300

    crate Bannerin AppointmentDetails

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
 
 // dependencies
 import { BorderlessButton } from 'react-native-gesture-handler';
@@ -8,6 +8,9 @@ import { Fontisto } from '@expo/vector-icons';
 // components
 import { Background } from '../../components/Background';
 import { Header } from '../../components/Header';
+
+// assets
+import BannerImg from '../../assets/banner.png';
 
 // styles
 import { styles } from './styles';
@@ -28,6 +31,25 @@ export function AppointmentDetails() {
           </BorderlessButton>
         }
       />
+      
+      <ImageBackground
+        source={BannerImg}
+        style={styles.banner}
+      >
+        <View style={styles.bannerContent} >
+          
+          <Text style={styles.title} >
+            Lendários
+          </Text>
+          
+          <Text style={styles.subTitle} >
+            É hoje que vamos chegar ao challenger sem perder uma partida da md10
+          </Text>
+
+        </View>
+
+      </ImageBackground>
+
     </Background>
   );
 }
