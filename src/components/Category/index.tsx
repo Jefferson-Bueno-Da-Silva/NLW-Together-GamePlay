6a04e650d7245b084416e28b8ac0a18b7c8b1d59
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-06-26 10:13:04 -0300

    interface adjustments, gradient in category

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -14,15 +14,17 @@ type Props = RectButtonProps & {
     title: string;
     icon: React.FC<SvgProps>;
     checked?: boolean;
+    hasCheckBox?: boolean;
 }
 
 export function Category({
     title,
     icon: Icon,
     checked = false,
+    hasCheckBox = false,
     ...rest
 }: Props) {
-    const { secondary50, secondary70 } = theme.colors;
+    const { secondary40, secondary50, secondary70, secondary85 } = theme.colors;
     
     return (
         <RectButton {...rest} >
@@ -30,11 +32,16 @@ export function Category({
                 style={styles.container}
                 colors={[secondary50, secondary70]}
             >
-                <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]} >
-                    
-                    <View 
-                        style={checked ? styles.checked : styles.check} 
-                    />
+                <LinearGradient 
+                    style={[styles.content, { opacity: checked ? 1 : 0.5 }]} 
+                    colors={[ checked ? secondary85 : secondary50, secondary40]}
+                >
+                    {
+                        hasCheckBox &&
+                        <View 
+                            style={checked ? styles.checked : styles.check} 
+                        />
+                    }
                     
                     <Icon 
                         width={48} 
@@ -45,7 +52,7 @@ export function Category({
                         { title }
                     </Text>
 
-                </View>
+                </LinearGradient>
             </LinearGradient>
         </RectButton>
     )
