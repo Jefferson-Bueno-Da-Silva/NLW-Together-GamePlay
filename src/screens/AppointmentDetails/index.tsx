ee5231a5b0b8f55b608f3d8f14da573822b965c8
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-06-26 11:40:09 -0300

    add header to AppointmentDetails

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import { View } from 'react-native';
 
+// dependencies
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Fontisto } from '@expo/vector-icons';
+
 // components
 import { Background } from '../../components/Background';
 import { Header } from '../../components/Header';
 
 // styles
 import { styles } from './styles';
+import { theme } from '../../global/styles/theme';
 
-export function AppointmentDetails(){
+export function AppointmentDetails() {
   return (
     <Background>
-        <Header
-            title="Detalhes"
-        />
+      <Header
+        title="Detalhes"
+        action={
+          <BorderlessButton>
+            <Fontisto
+              name="share"
+              size={24}
+              color={theme.colors.primary}
+            />
+          </BorderlessButton>
+        }
+      />
     </Background>
   );
 }
