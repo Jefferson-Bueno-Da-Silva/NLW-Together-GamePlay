3d971c8a25a90074e3035e1962af19e82dd7d1d8
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-06-26 11:10:34 -0300

    create component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,50 @@
+import React, { ReactNode } from 'react';
+import { View, Text } from 'react-native';
+
+// dependencies
+import { LinearGradient } from 'expo-linear-gradient';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
+
+// components
+
+
+// styles
+import { styles } from './styles';
+import { theme } from '../../global/styles/theme';
+
+
+type Props = {
+    title: string;
+    action?: ReactNode;
+}
+
+export function Header({ title, action }: Props){
+    const { secondary100, secondary40, heading } = theme.colors;
+
+    return (
+        <LinearGradient
+            style={styles.container}
+            colors={[secondary100, secondary40]}
+        >
+            <BorderlessButton>
+                <Feather
+                    name="arrow-left"
+                    size={24}
+                    color={heading}
+                />
+            </BorderlessButton>
+
+            <Text style={styles.title} >
+                {title}
+            </Text>
+
+            {
+                action &&
+                <View></View>
+            }
+
+        </LinearGradient>
+    );
+}
+
